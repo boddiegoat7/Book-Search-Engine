a0b128dc563ef54efa1f10b63008c5bedb07afc6
victor boddie <victorboddie21@gmail.com>
2022-11-15 00:16:57 -0500

    adde schema folder and finsihed up the typedef files and the athication needed to match the malware.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,19 +3,50 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-  type User {
+
+type Query {
+    me: User
+}
+
+type User {
     _id: ID
     username: String
     email: String
-    book: String
-    
-  }
+    bookCount: Int
+    savedBooks: [Book]
+}
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID
+    user: User
+}
+
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
+
+}
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
