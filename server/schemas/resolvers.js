7ed85775d2cc8ef49188c8080d10c4ab597ec75f
victor boddie <victorboddie21@gmail.com>
2022-11-15 21:45:04 -0500

    added mutations file to utlis folder to get queries for log in, user, savebook, and remove books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -87,7 +87,7 @@ const resolvers = {
         
             return updatedUser;
 
-        }
+            }
     
         },
     
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,10 +41,10 @@ type Auth {
 
 
 type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(newBook: InputBook!): User
-    removeBook(bookId: ID!): User
+    login(email: String, password: String): Auth
+    addUser(username: String, email: String, password: String): Auth
+    saveBook(newBook: InputBook): User
+    removeBook(bookId: ID): User
 
 }
 `;
